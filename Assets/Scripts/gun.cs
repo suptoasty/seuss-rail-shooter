095c14e6cad6d062a983aaddf72715dcb5ebb77a
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2020-08-22 12:42:25 -0500

    Added files for commit that were not added for some reason.

---
+++ Assets/Scripts/gun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Transform fireLocation = gameObject.transform.GetChild(0);
+            GameObject temp = Instantiate(projectile, fireLocation.position, fireLocation.rotation);
+            temp.GetComponent<Rigidbody>().AddForce(transform.forward * 1000, ForceMode.Force);
+            StartCoroutine(destroyTimer(2, temp));
+        }
+    }
+    
+    IEnumerator destroyTimer(int time, GameObject bullet)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(bullet);
+    }
+}
