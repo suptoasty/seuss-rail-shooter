86c23a23b715578fd2423390da718b4e6265a086
suptoasty <jason.lonsinger@gmail.com>
2020-08-26 09:59:10 -0500

    Added start of GameManger and Portal For transitions

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+    private int score = 0;
+    private int lives = 0;
+    public int maxLives = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetScore();
+    }
+
+    public void decreaseLives() {
+        lives--;
+    }
+
+    public void resetLives() {
+        lives = maxLives;
+    }
+
+    public void addScore(int amount) {
+        score += amount;
+    }
+
+    public void incrementScore() {
+        score++;
+    }
+
+    public void resetScore() {
+        score = 0;
+    }
+}
---
+++ Assets/Scripts/playerPortal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerPortal : MonoBehaviour
+{
+    public GameObject newLocation;
+
+    // // private void Start() {
+    // // }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            Debug.Log("Player Entered Portal");
+            other.gameObject.transform.position = newLocation.gameObject.transform.position;
+
+        }
+    }
+
+
+}
