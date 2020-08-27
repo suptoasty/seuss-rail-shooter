1da2be03b37a5e26154180ed0c37565025313d90
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2020-08-27 11:20:51 -0500

    Set up main menu
    Set up Options, High Scores, Play, and Quit menus/buttons.
    Added resolution and audio control.
    
    Added player controller to inside of tent, scaled.
    
    Added some collision to inside of tent.
    Fixed gun rotation to center screen.
    Added crosshair.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject homeScreen;
+    public GameObject optionsScreen;
+    public GameObject highscoresScreen;
+    public GameObject resolutionPicker;
+    public GameObject volumeSlider;
+    bool fullScreen;
+    int width;
+    int height;
+
+    private void Start()
+    {
+        //This is required otherwise objects do not exist if you begin with them disabled / invisable.
+        highscoresScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+
+        width = 1920;
+        height = 1080;
+        fullScreen = false;
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void HomeDisplay()
+    {
+        highscoresScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+        homeScreen.SetActive(true);
+    }
+
+    public void HighScoresDisplay()
+    {
+        homeScreen.SetActive(false);
+        highscoresScreen.SetActive(true);
+    }
+
+    public void OptionsDisplay()
+    {
+        homeScreen.SetActive(false);
+        optionsScreen.SetActive(true);
+    }
+
+    public void changeResolution()
+    {
+        string temp = resolutionPicker.GetComponent<TMP_Dropdown>().options[resolutionPicker.GetComponent<TMP_Dropdown>().value].text;
+        Debug.Log(temp);
+        width = Int32.Parse(temp.Split(' ')[0]);
+        height = Int32.Parse(temp.Split(' ')[2]);
+        Screen.SetResolution(width, height, fullScreen);
+    }
+
+    public void toggleFullScreen()
+    {
+        fullScreen = !fullScreen;
+        Screen.SetResolution(width, height, fullScreen);
+    }
+
+    public void changeVolume()
+    {
+        //Need to set up an Audio mixer with a master volume, then tie this to that
+        //AudioSource.volume = volumeSlider.GetComponent<Slider>().value;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
