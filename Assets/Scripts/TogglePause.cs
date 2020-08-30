a18527d687118f75d0bd87521e176e529f3e29bc
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2020-08-30 13:30:16 -0500

    Moved main menu UI over to main menu scene.
    Added pause menu

---
+++ Assets/Scripts/TogglePause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePause : MonoBehaviour
+{
+    // Start is called before the first frame update.
+
+    public GameObject pauseMenu;
+    public GameObject optionsMenu;
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            pauseMenu.SetActive(!pauseMenu.activeInHierarchy);
+        }
+    }
+}
