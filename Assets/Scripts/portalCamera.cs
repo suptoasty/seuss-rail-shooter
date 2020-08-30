d0f4f098934fa2599e2918ad7f9c3a606347a0f7
suptoasty <jason.lonsinger@gmail.com>
2020-08-30 12:41:52 -0500

    Added camera script to fix portal texture projection and rotation

---
+++ Assets/Scripts/portalCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.Rendering;
+
+public class portalCamera : MonoBehaviour
+{
+    void Update() {
+        UpdateCamera(this.GetComponent<Camera>());
+        transform.forward = Camera.main.transform.forward;
+    }
+
+    void UpdateCamera(Camera camera) {
+        camera.projectionMatrix = Camera.main.projectionMatrix;
+    }
+
+
+}
